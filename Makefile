7ecf2ca73aee1d569eda025b80aa5491ebde1be6
c14006078 <c14006078@mail.ncku.edu.tw>
2016-09-25 03:34:10 +0800

    remove unnecessary comment and code.

--- Makefile
+++ Makefile
@@ -1,24 +1,14 @@
 CC ?= gcc
 CFLAGS_common ?= -Wall -std=gnu99
 CFLAGS = -O0 -g
-#CPP = g++
-#CXXFLAGS_common ?= -Wall -std=c++11
-#CXXFLAGS = -O0 -g
 
 EXEC = clz_recursive clz_iterative clz_binary_search clz_byte_shift
 all: $(EXEC)
 
 SRCS_common = test.c
 
-#clz_recursive.o: clz_recursive.cpp
-#	$(CPP) $(CXXFLAGS_common) $(CXXFLAGS) -c $^
-
 %.o: %.c
 	$(CC) $(CFLAGS_common) $(CFLAGS) -c $^
-
-#clz_recursive: $(SRCS_common) clz_recursive.o
-#	$(CPP) $(CXXFLAGS_common) $(CXXFLAGS) \
-#	 -o $@ $(SRCS_common) $@.o
 
 clz_recursive: $(SRCS_common) clz_recursive.o
 	$(CC) $(CFLAGS_common) $(CFLAGS_opt) \
--- test.c
+++ test.c
@@ -8,35 +8,9 @@
 
 int main( int argc, char** argv)
 {
-
-	//assert( clz( 0x00123456) == 8 && "0x00123456 clz error\n");
-	/*ASSERT_CLZ( 0xFFFFFFFF, 0);
-	ASSERT_CLZ( 0x7FFFFFFF, 1);
-	ASSERT_CLZ( 0x3FFFFFFF, 2);
-	ASSERT_CLZ( 0x1FFFFFFF, 3);
-	ASSERT_CLZ( 0x0FFFFFFF, 4);
-	ASSERT_CLZ( 0x07FFFFFF, 5);
-	ASSERT_CLZ( 0x03FFFFFF, 6);
-	ASSERT_CLZ( 0x01FFFFFF, 7);
-	ASSERT_CLZ( 0x00FFFFFF, 8);
-	ASSERT_CLZ( 0x007FFFFF, 9);
-	ASSERT_CLZ( 0x003FFFFF,10);
-	ASSERT_CLZ( 0x001FFFFF,11);
-	ASSERT_CLZ( 0x000FFFFF,12);
-	ASSERT_CLZ( 0x0007FFFF,13);
-	ASSERT_CLZ( 0x0003FFFF,14);
-	ASSERT_CLZ( 0x0001FFFF,15);
-	ASSERT_CLZ( 0x0000FFFF, 16);*/
-
-	/*ASSERT_CLZ( 0, 32);
-	ASSERT_CLZ( 1, 31);
-	ASSERT_CLZ( 2, 30);
-	ASSERT_CLZ( 4, 29);*/
 	for( int i = 0; i < 32; i++){
 		int in = 1 << i, ans = 31 -i;
-		//printf("in = %d, ans = %d", in, ans);
 		ASSERT_CLZ( in, ans);
-		//assert( clz( in) == ans);
 	}
 		
 }
